43f7af7d9b6d8b22082ae94f9b4a59b455e69fb3
Ariful Alam <swazan.arif@gmail.com>
2022-03-29 22:05:45 +0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@
 module.exports = {
   env: {
     browser: true,
+    es2021: true,
     node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 };
